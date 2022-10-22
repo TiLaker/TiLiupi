095f3ab82fc19d9b111c4f768868d376d4632e70
Leonard Xu <xbjtdcq@gmail.com>
2022-10-22 21:19:00 +0800

    [ti-liupi] Add tidb-cdc connector which base on Flink 1.15

---
+++ cdc/flink-cdc-connectors/flink-cdc-base/src/main/java/com/ververica/cdc/connectors/base/source/reader/external/JdbcSourceScanFetcher.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2022 Ververica Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.base.source.reader.external;
+
+import org.apache.flink.util.FlinkRuntimeException;
+
+import org.apache.flink.shaded.guava30.com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import com.ververica.cdc.connectors.base.source.meta.split.SnapshotSplit;
+import com.ververica.cdc.connectors.base.source.meta.split.SourceRecords;
+import com.ververica.cdc.connectors.base.source.meta.split.SourceSplitBase;
+import io.debezium.connector.base.ChangeEventQueue;
+import io.debezium.pipeline.DataChangeEvent;
+import io.debezium.util.SchemaNameAdjuster;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.formatMessageTimestamp;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.getSplitKey;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isDataChangeRecord;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isEndWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isHighWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.isLowWatermarkEvent;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.splitKeyRangeContains;
+import static com.ververica.cdc.connectors.base.utils.SourceRecordUtils.upsertBinlog;
+import static org.apache.flink.util.Preconditions.checkState;
+
+/**
+ * Fetcher to fetch data from table split, the split is the snapshot split {@link SnapshotSplit}.
+ */
+public class JdbcSourceScanFetcher implements Fetcher<SourceRecords, SourceSplitBase> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JdbcSourceScanFetcher.class);
+
+    public AtomicBoolean hasNextElement;
+    public AtomicBoolean reachEnd;
+
+    private final JdbcSourceFetchTaskContext taskContext;
+    private final ExecutorService executorService;
+    private volatile ChangeEventQueue<DataChangeEvent> queue;
+    private volatile Throwable readException;
+
+    // task to read snapshot for current split
+    private FetchTask<SourceSplitBase> snapshotSplitReadTask;
+    private SnapshotSplit currentSnapshotSplit;
+    private SchemaNameAdjuster nameAdjuster;
+
+    private static final long READER_CLOSE_TIMEOUT_SECONDS = 30L;
+
+    public JdbcSourceScanFetcher(JdbcSourceFetchTaskContext taskContext, int subtaskId) {
+        this.taskContext = taskContext;
+        ThreadFactory threadFactory =
+                new ThreadFactoryBuilder()
+                        .setNameFormat("debezium-snapshot-reader-" + subtaskId)
+                        .build();
+        this.executorService = Executors.newSingleThreadExecutor(threadFactory);
+        this.hasNextElement = new AtomicBoolean(false);
+        this.reachEnd = new AtomicBoolean(false);
+    }
+
+    @Override
+    public void submitTask(FetchTask<SourceSplitBase> fetchTask) {
+        this.snapshotSplitReadTask = fetchTask;
+        this.currentSnapshotSplit = fetchTask.getSplit().asSnapshotSplit();
+        taskContext.configure(currentSnapshotSplit);
+        this.nameAdjuster = taskContext.getSchemaNameAdjuster();
+        this.queue = taskContext.getQueue();
+        this.hasNextElement.set(true);
+        this.reachEnd.set(false);
+        executorService.submit(
+                () -> {
+                    try {
+                        snapshotSplitReadTask.execute(taskContext);
+                    } catch (Exception e) {
+                        LOG.error(
+                                String.format(
+                                        "Execute snapshot read task for snapshot split %s fail",
+                                        currentSnapshotSplit),
+                                e);
+                        readException = e;
+                    }
+                });
+    }
+
+    @Override
+    public boolean isFinished() {
+        return currentSnapshotSplit == null
+                || (!snapshotSplitReadTask.isRunning() && !hasNextElement.get() && reachEnd.get());
+    }
+
+    @Nullable
+    @Override
+    public Iterator<SourceRecords> pollSplitRecords() throws InterruptedException {
+        checkReadException();
+
+        if (hasNextElement.get()) {
+            // data input: [low watermark event][snapshot events][high watermark event][binlog
+            // events][binlog-end event]
+            // data output: [low watermark event][normalized events][high watermark event]
+            boolean reachBinlogStart = false;
+            boolean reachBinlogEnd = false;
+            SourceRecord lowWatermark = null;
+            SourceRecord highWatermark = null;
+            Map<Struct, SourceRecord> snapshotRecords = new HashMap<>();
+            while (!reachBinlogEnd) {
+                checkReadException();
+                List<DataChangeEvent> batch = queue.poll();
+                for (DataChangeEvent event : batch) {
+                    SourceRecord record = event.getRecord();
+                    if (lowWatermark == null) {
+                        lowWatermark = record;
+                        assertLowWatermark(lowWatermark);
+                        continue;
+                    }
+
+                    if (highWatermark == null && isHighWatermarkEvent(record)) {
+                        highWatermark = record;
+                        // snapshot events capture end and begin to capture binlog events
+                        reachBinlogStart = true;
+                        continue;
+                    }
+
+                    if (reachBinlogStart && isEndWatermarkEvent(record)) {
+                        // capture to end watermark events, stop the loop
+                        reachBinlogEnd = true;
+                        break;
+                    }
+
+                    if (!reachBinlogStart) {
+                        snapshotRecords.put((Struct) record.key(), record);
+                    } else {
+                        if (isRequiredBinlogRecord(record)) {
+                            // upsert binlog events through the record key
+                            upsertBinlog(snapshotRecords, record);
+                        }
+                    }
+                }
+            }
+            // snapshot split return its data once
+            hasNextElement.set(false);
+
+            final List<SourceRecord> normalizedRecords = new ArrayList<>();
+            normalizedRecords.add(lowWatermark);
+            normalizedRecords.addAll(formatMessageTimestamp(snapshotRecords.values()));
+            normalizedRecords.add(highWatermark);
+
+            final List<SourceRecords> sourceRecordsSet = new ArrayList<>();
+            sourceRecordsSet.add(new SourceRecords(normalizedRecords));
+            return sourceRecordsSet.iterator();
+        }
+        // the data has been polled, no more data
+        reachEnd.compareAndSet(false, true);
+        return null;
+    }
+
+    private void checkReadException() {
+        if (readException != null) {
+            throw new FlinkRuntimeException(
+                    String.format(
+                            "Read split %s error due to %s.",
+                            currentSnapshotSplit, readException.getMessage()),
+                    readException);
+        }
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (executorService != null) {
+                executorService.shutdown();
+                if (executorService.awaitTermination(
+                        READER_CLOSE_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+                    LOG.warn(
+                            "Failed to close the scan fetcher in {} seconds.",
+                            READER_CLOSE_TIMEOUT_SECONDS);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Close scan fetcher error", e);
+        }
+    }
+
+    private void assertLowWatermark(SourceRecord lowWatermark) {
+        checkState(
+                isLowWatermarkEvent(lowWatermark),
+                String.format(
+                        "The first record should be low watermark signal event, but actual is %s",
+                        lowWatermark));
+    }
+
+    private boolean isRequiredBinlogRecord(SourceRecord record) {
+        if (isDataChangeRecord(record)) {
+            Object[] key =
+                    getSplitKey(currentSnapshotSplit.getSplitKeyType(), record, nameAdjuster);
+            return splitKeyRangeContains(
+                    key, currentSnapshotSplit.getSplitStart(), currentSnapshotSplit.getSplitEnd());
+        }
+        return false;
+    }
+}
---
+++ cdc/flink-cdc-connectors/flink-cdc-base/src/main/java/com/ververica/cdc/connectors/base/utils/SourceRecordUtils.java
@@ -0,0 +1,311 @@
+/*
+ * Copyright 2022 Ververica Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ververica.cdc.connectors.base.utils;
+
+import org.apache.flink.table.types.logical.RowType;
+
+import com.ververica.cdc.connectors.base.relational.JdbcSourceEventDispatcher.WatermarkKind;
+import io.debezium.data.Envelope;
+import io.debezium.document.DocumentReader;
+import io.debezium.relational.TableId;
+import io.debezium.relational.history.HistoryRecord;
+import io.debezium.util.SchemaNameAdjuster;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.ververica.cdc.connectors.base.relational.JdbcSourceEventDispatcher.HISTORY_RECORD_FIELD;
+import static com.ververica.cdc.connectors.base.relational.JdbcSourceEventDispatcher.SIGNAL_EVENT_VALUE_SCHEMA_NAME;
+import static com.ververica.cdc.connectors.base.relational.JdbcSourceEventDispatcher.WATERMARK_KIND;
+import static io.debezium.connector.AbstractSourceInfo.DATABASE_NAME_KEY;
+import static io.debezium.connector.AbstractSourceInfo.SCHEMA_NAME_KEY;
+import static io.debezium.connector.AbstractSourceInfo.TABLE_NAME_KEY;
+
+/** Utility class to deal record. */
+public class SourceRecordUtils {
+
+    private SourceRecordUtils() {}
+
+    public static final String SCHEMA_CHANGE_EVENT_KEY_NAME =
+            "io.debezium.connector.mysql.SchemaChangeKey";
+    private static final DocumentReader DOCUMENT_READER = DocumentReader.defaultReader();
+
+    /** Converts a {@link ResultSet} row to an array of Objects. */
+    public static Object[] rowToArray(ResultSet rs, int size) throws SQLException {
+        final Object[] row = new Object[size];
+        for (int i = 0; i < size; i++) {
+            row[i] = rs.getObject(i + 1);
+        }
+        return row;
+    }
+
+    public static boolean isWatermarkEvent(SourceRecord record) {
+        Optional<WatermarkKind> watermarkKind = getWatermarkKind(record);
+        return watermarkKind.isPresent();
+    }
+
+    public static boolean isLowWatermarkEvent(SourceRecord record) {
+        Optional<WatermarkKind> watermarkKind = getWatermarkKind(record);
+        return watermarkKind.isPresent() && watermarkKind.get() == WatermarkKind.LOW;
+    }
+
+    public static boolean isHighWatermarkEvent(SourceRecord record) {
+        Optional<WatermarkKind> watermarkKind = getWatermarkKind(record);
+        return watermarkKind.isPresent() && watermarkKind.get() == WatermarkKind.HIGH;
+    }
+
+    public static boolean isEndWatermarkEvent(SourceRecord record) {
+        Optional<WatermarkKind> watermarkKind = getWatermarkKind(record);
+        return watermarkKind.isPresent() && watermarkKind.get() == WatermarkKind.BINLOG_END;
+    }
+
+    /**
+     * Return the timestamp when the change event is produced in MySQL.
+     *
+     * <p>The field `source.ts_ms` in {@link SourceRecord} data struct is the time when the change
+     * event is operated in MySQL.
+     */
+    public static Long getMessageTimestamp(SourceRecord record) {
+        Schema schema = record.valueSchema();
+        Struct value = (Struct) record.value();
+        if (schema.field(Envelope.FieldName.SOURCE) == null) {
+            return null;
+        }
+
+        Struct source = value.getStruct(Envelope.FieldName.SOURCE);
+        if (source.schema().field(Envelope.FieldName.TIMESTAMP) == null) {
+            return null;
+        }
+
+        return source.getInt64(Envelope.FieldName.TIMESTAMP);
+    }
+
+    /**
+     * The field `ts_ms` in {@link SourceRecord} data struct is the time when the record fetched by
+     * debezium reader, use it as the process time in Source.
+     */
+    public static Long getFetchTimestamp(SourceRecord record) {
+        Schema schema = record.valueSchema();
+        Struct value = (Struct) record.value();
+        if (schema.field(Envelope.FieldName.TIMESTAMP) == null) {
+            return null;
+        }
+        return value.getInt64(Envelope.FieldName.TIMESTAMP);
+    }
+
+    public static boolean isSchemaChangeEvent(SourceRecord sourceRecord) {
+        Schema keySchema = sourceRecord.keySchema();
+        return keySchema != null && SCHEMA_CHANGE_EVENT_KEY_NAME.equalsIgnoreCase(keySchema.name());
+    }
+
+    public static boolean isDataChangeRecord(SourceRecord record) {
+        Schema valueSchema = record.valueSchema();
+        Struct value = (Struct) record.value();
+        return valueSchema.field(Envelope.FieldName.OPERATION) != null
+                && value.getString(Envelope.FieldName.OPERATION) != null;
+    }
+
+    public static TableId getTableId(SourceRecord dataRecord) {
+        Struct value = (Struct) dataRecord.value();
+        Struct source = value.getStruct(Envelope.FieldName.SOURCE);
+        String dbName = source.getString(DATABASE_NAME_KEY);
+        // Oracle need schemaName
+        String schemaName = getSchemaName(source);
+        String tableName = source.getString(TABLE_NAME_KEY);
+        return new TableId(dbName, schemaName, tableName);
+    }
+
+    public static String getSchemaName(Struct source) {
+        if (source.schema().fields().stream().anyMatch(r -> SCHEMA_NAME_KEY.equals(r.name()))) {
+            return source.getString(SCHEMA_NAME_KEY);
+        }
+        return null;
+    }
+
+    public static Object[] getSplitKey(
+            RowType splitBoundaryType, SourceRecord dataRecord, SchemaNameAdjuster nameAdjuster) {
+        // the split key field contains single field now
+        String splitFieldName = nameAdjuster.adjust(splitBoundaryType.getFieldNames().get(0));
+        Struct key = (Struct) dataRecord.key();
+        return new Object[] {key.get(splitFieldName)};
+    }
+
+    /** Returns the specific key contains in the split key range or not. */
+    public static boolean splitKeyRangeContains(
+            Object[] key, Object[] splitKeyStart, Object[] splitKeyEnd) {
+        // for all range
+        if (splitKeyStart == null && splitKeyEnd == null) {
+            return true;
+        }
+        // first split
+        if (splitKeyStart == null) {
+            int[] upperBoundRes = new int[key.length];
+            for (int i = 0; i < key.length; i++) {
+                upperBoundRes[i] = compareObjects(key[i], splitKeyEnd[i]);
+            }
+            return Arrays.stream(upperBoundRes).anyMatch(value -> value < 0)
+                    && Arrays.stream(upperBoundRes).allMatch(value -> value <= 0);
+        }
+        // last split
+        else if (splitKeyEnd == null) {
+            int[] lowerBoundRes = new int[key.length];
+            for (int i = 0; i < key.length; i++) {
+                lowerBoundRes[i] = compareObjects(key[i], splitKeyStart[i]);
+            }
+            return Arrays.stream(lowerBoundRes).allMatch(value -> value >= 0);
+        }
+        // other split
+        else {
+            int[] lowerBoundRes = new int[key.length];
+            int[] upperBoundRes = new int[key.length];
+            for (int i = 0; i < key.length; i++) {
+                lowerBoundRes[i] = compareObjects(key[i], splitKeyStart[i]);
+                upperBoundRes[i] = compareObjects(key[i], splitKeyEnd[i]);
+            }
+            return Arrays.stream(lowerBoundRes).anyMatch(value -> value >= 0)
+                    && (Arrays.stream(upperBoundRes).anyMatch(value -> value < 0)
+                            && Arrays.stream(upperBoundRes).allMatch(value -> value <= 0));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static int compareObjects(Object o1, Object o2) {
+        if (o1 instanceof Comparable && o1.getClass().equals(o2.getClass())) {
+            return ((Comparable) o1).compareTo(o2);
+        } else if (isNumericObject(o1) && isNumericObject(o2)) {
+            return toBigDecimal(o1).compareTo(toBigDecimal(o2));
+        } else {
+            return o1.toString().compareTo(o2.toString());
+        }
+    }
+
+    private static boolean isNumericObject(Object obj) {
+        return obj instanceof Byte
+                || obj instanceof Short
+                || obj instanceof Integer
+                || obj instanceof Long
+                || obj instanceof Float
+                || obj instanceof Double
+                || obj instanceof BigInteger
+                || obj instanceof BigDecimal;
+    }
+
+    private static BigDecimal toBigDecimal(Object numericObj) {
+        return new BigDecimal(numericObj.toString());
+    }
+
+    public static HistoryRecord getHistoryRecord(SourceRecord schemaRecord) throws IOException {
+        Struct value = (Struct) schemaRecord.value();
+        String historyRecordStr = value.getString(HISTORY_RECORD_FIELD);
+        return new HistoryRecord(DOCUMENT_READER.read(historyRecordStr));
+    }
+
+    private static Optional<WatermarkKind> getWatermarkKind(SourceRecord record) {
+        if (record.valueSchema() != null
+                && SIGNAL_EVENT_VALUE_SCHEMA_NAME.equals(record.valueSchema().name())) {
+            Struct value = (Struct) record.value();
+            return Optional.of(WatermarkKind.valueOf(value.getString(WATERMARK_KIND)));
+        }
+        return Optional.empty();
+    }
+
+    /** upsert binlog events to snapshot events collection. */
+    public static void upsertBinlog(
+            Map<Struct, SourceRecord> snapshotRecords, SourceRecord binlogRecord) {
+        Struct key = (Struct) binlogRecord.key();
+        Struct value = (Struct) binlogRecord.value();
+        if (value != null) {
+            Envelope.Operation operation =
+                    Envelope.Operation.forCode(value.getString(Envelope.FieldName.OPERATION));
+            switch (operation) {
+                case CREATE:
+                case UPDATE:
+                    Envelope envelope = Envelope.fromSchema(binlogRecord.valueSchema());
+                    Struct source = value.getStruct(Envelope.FieldName.SOURCE);
+                    Struct after = value.getStruct(Envelope.FieldName.AFTER);
+                    Instant fetchTs =
+                            Instant.ofEpochMilli((Long) source.get(Envelope.FieldName.TIMESTAMP));
+                    SourceRecord record =
+                            new SourceRecord(
+                                    binlogRecord.sourcePartition(),
+                                    binlogRecord.sourceOffset(),
+                                    binlogRecord.topic(),
+                                    binlogRecord.kafkaPartition(),
+                                    binlogRecord.keySchema(),
+                                    binlogRecord.key(),
+                                    binlogRecord.valueSchema(),
+                                    envelope.read(after, source, fetchTs));
+                    snapshotRecords.put(key, record);
+                    break;
+                case DELETE:
+                    snapshotRecords.remove(key);
+                    break;
+                case READ:
+                    throw new IllegalStateException(
+                            String.format(
+                                    "Binlog record shouldn't use READ operation, the the record is %s.",
+                                    binlogRecord));
+            }
+        }
+    }
+
+    /**
+     * Format message timestamp(source.ts_ms) value to 0L for all records read in snapshot phase.
+     */
+    public static List<SourceRecord> formatMessageTimestamp(
+            Collection<SourceRecord> snapshotRecords) {
+        return snapshotRecords.stream()
+                .map(
+                        record -> {
+                            Envelope envelope = Envelope.fromSchema(record.valueSchema());
+                            Struct value = (Struct) record.value();
+                            Struct updateAfter = value.getStruct(Envelope.FieldName.AFTER);
+                            // set message timestamp (source.ts_ms) to 0L
+                            Struct source = value.getStruct(Envelope.FieldName.SOURCE);
+                            source.put(Envelope.FieldName.TIMESTAMP, 0L);
+                            // extend the fetch timestamp(ts_ms)
+                            Instant fetchTs =
+                                    Instant.ofEpochMilli(
+                                            value.getInt64(Envelope.FieldName.TIMESTAMP));
+                            SourceRecord sourceRecord =
+                                    new SourceRecord(
+                                            record.sourcePartition(),
+                                            record.sourceOffset(),
+                                            record.topic(),
+                                            record.kafkaPartition(),
+                                            record.keySchema(),
+                                            record.key(),
+                                            record.valueSchema(),
+                                            envelope.read(updateAfter, source, fetchTs));
+                            return sourceRecord;
+                        })
+                .collect(Collectors.toList());
+    }
+}
